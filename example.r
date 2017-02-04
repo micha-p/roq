6463a6446ee424d7c524452e8b95aa229984d344
micha-p <you@example.com>
2017-02-04 16:18:26 +0100

    Conditionals do not need parentheses (in contrast to R)

--- example.r
+++ example.r
@@ -1,32 +1,4 @@
-f<-function(a,b)a+b
-f(1,2)
-#[1] 3
 
-f<-function(a,b) a+b
-f(1,2)
-#[1] 3
-
-f<-function(a,b) a+b; 10+11
-#[1] 21
-
-f(1,2)
-#[1] 3
-
-a<-133
-f<-function(a,b){a<-2}
-f(1,2)
-a
-#[1] 133
-
-
-f<-function(a,b){if(a)a else 33}
-f(1,2)
-#[1] 1
-f(0,2)
-#[1] 33
- 
-f<-function(a,b)a<-2
-f(1,2)
-f<-function(a,b)if(a)a else 33
-f(1,2)
-# [1] 1
+if 3 4 else 5
+if 3+4 4 else 5
+if 3-3 4 else 5
