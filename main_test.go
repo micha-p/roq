148da79bdf3293226b6f1780966c0a85a4c82a1c
micha-p <you@example.com>
2017-12-04 20:42:48 +0100

    Testing for arithmetic operators

--- main_test.go
+++ main_test.go
@@ -13,8 +13,14 @@ func TestOK(t *testing.T) {
 	}
 }
 
-func ExampleAdd() {
-        eval.EvalTest("1+2")
+func ExampleArithmeticOperators() {
+        eval.EvalStringForTest("1+2\n2*3.1\n5-1\n7/8\n11%%2\n3^2")
         // Output:
         // [1] 3
+	// [1] 6.2
+	// [1] 4
+	// [1] 0.875
+	// [1] 1
+	// [1] 9
+
 }
