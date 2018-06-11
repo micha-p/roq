0cfa230fab0637e1a055bc554781e6798c441e9a
micha-p <you@example.com>
2018-06-11 22:48:23 +0200

    Release 0.1.6

--- version/version.go
+++ version/version.go
@@ -3,8 +3,10 @@ package version
 import "runtime"
 
 const MAJOR = "0"
-const MINOR = "1.5"
-
+const MINOR = "1.6"
+const YEAR = 2018
+const MONTH = 06
+const DAY = 11
 
 func PrintVersion() {
 	print("platform       "); println(runtime.GOARCH + "-pc-" + runtime.GOOS)
@@ -13,6 +15,9 @@ func PrintVersion() {
 	print("status         "); println("proof of concept")
 	print("major          "); println(MAJOR)
 	print("minor          "); println(MINOR)
+	print("year           "); println(YEAR)
+	print("month          "); println(MONTH)
+	print("day            "); println(DAY)
 	print("language       "); println("roq")
 	print("nickname       "); println("R core in go")
 	return
@@ -24,17 +29,11 @@ func PrintVersion() {
 //system         x86_64, linux-gnu           
 //status                                     
 //major          3                           
-//minor          2.3                         
-//year           2015                        
-//month          12                          
-//day            10                          
-//svn rev        69752                       
+//minor          4.4                         
+//year           2018                        
+//month          03                          
+//day            15                          
+//svn rev        74408                       
 //language       R                           
-//version.string R version 3.2.3 (2015-12-10)
-//nickname       Wooden Christmas-Tree       
-
-
-//R --version
-//R version 3.4.3 (2017-11-30) -- "Kite-Eating Tree"
-//Copyright (C) 2017 The R Foundation for Statistical Computing
-//Platform: x86_64-pc-linux-gnu (64-bit)
+//version.string R version 3.4.4 (2018-03-15)
+//nickname       Someone to Lean On      
