fd70db8aa4d71ccb90fde4471d8da156e5a7b6be
micha-p <you@example.com>
2018-10-25 00:37:44 +0200

    Indexing tests

--- index_test.go
+++ index_test.go
@@ -5,7 +5,6 @@ import (
 )
 
 // indexing an array returns a slice
-
 func TestIndex(t *testing.T) {
 	quicktestSlice(t, "a=c(11,22,33,44,55,66); a[1]", []float64{11}, 0)
 	quicktestSlice(t, "a=c(11,22,33,44,55,66); a[1.1]", []float64{11}, 0)
@@ -20,12 +19,15 @@ func TestListIndex(t *testing.T) {
 
 func TestIndexSlicing(t *testing.T) {
 	quicktestSlice(t, "a=c(11,22,33,44,55,66); a[2:4]", []float64{22,33,44}, 0)
+	quicktestSlice(t, "a=c(11,22,33,44,55,66); b=c(1,3,6);a[b]", []float64{11,33,66}, 0)
+	quicktestSlice(t, "a=c(11,22,33,44,55,66); b=c(1,3);a[2*b]", []float64{22,66}, 0)
 }
+
 
 func TestChainedIndex(t *testing.T) {
 	quicktestSlice(t, "a=c(11,22,33,44,55,66); a[2:4][2]",[]float64{33}, 0)
 	quicktestValue(t, "a=list(11,list(22,23,24),33,44,55,66); a[[2]][[3]]",24,0)
-//	quicktestValue(t, "a=list(11,list(22,23,24,c(100,101,102)),33,44,55,66);a[[2]][[4]][2]",101,0)
+	quicktestSlice(t, "a=list(11,list(22,23,24,c(100,101,102)),33,44,55,66);a[[2]][[4]][2]",[]float64{101},0)
 }
 
 
